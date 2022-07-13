04ffbaf60ab1f436d8bea3dc00047107861b7e22
dipak-tomar <dipakkumartomar758@gmail.com>
2022-07-13 18:57:52 +0530

    person controller completed

---
+++ controller/PersonController.js
@@ -0,0 +1,32 @@
+const excelToJson = require("convert-excel-to-json");
+const Person = require("../model/personSchema");
+
+module.exports = async function uploadController(req, res) {
+  await Person.deleteMany({});
+  const excelData = excelToJson({
+    source: req.file.buffer,
+    sheets: [
+      {
+        name: "Sheet1",
+        header: {
+          rows: 1,
+        },
+        columnToKey: {
+          A: "Name of the Candidate",
+          B: "Email",
+          C: "Mobile No.",
+          D: "Date of Birth",
+          E: "Work Experience",
+          F: "Resume Title",
+          G: "Current Location",
+          H: "Postal Address",
+          I: "Current Employer",
+          J: "Current Designation",
+        },
+      },
+    ],
+  });
+  await Person.insertMany(excelData.Sheet1, { ordered: false }, () => {
+    res.redirect("http://localhost:8000/thankyou.html");
+  });
+};
