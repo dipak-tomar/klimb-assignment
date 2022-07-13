db4abbef866162f7ce3f654c46c5bb157275579e
dipak-tomar <dipakkumartomar758@gmail.com>
2022-07-13 18:53:38 +0530

    person model completed

---
+++ models/Person.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const personSchema = new mongoose.Schema({
+  "Name of the Candidate": String,
+  Email: {
+    type: String,
+    unique: true,
+  },
+  "Mobile No.": String,
+  "Date of Birth": String,
+  "Work Experience": String,
+  "Resume Title": String,
+  "Current Location": String,
+  "Postal Address": String,
+  "Current Employer": String,
+  "Current Designation": String,
+});
+
+
+const Person =  mongoose.model("Person", personSchema);
+
+module.exports = Person;
